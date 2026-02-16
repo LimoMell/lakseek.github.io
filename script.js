9e9b5321dd7a8de0453b9bc98d2f995147b68375
lm <limomell0807@gmail.com>
2026-02-17 02:29:03 +0800

    Eating cup noodle

--- script.js
+++ script.js
@@ -428,38 +428,6 @@
     });
 })();
 
-// 彩蛋觸發：點更新日期 6 次
-(function () {
-    const THRESHOLD = 6;
-    const RESET_DELAY = 1500;
-    const updateDateBaka = document.getElementById("updateDateBaka");
-
-    if (!updateDateBaka) return;
-
-    let count = 0;
-    let resetTimer = null;
-
-    updateDateBaka.addEventListener("click", () => {
-        count++;
-        clearTimeout(resetTimer);
-
-        if (count >= THRESHOLD) {
-            count = 0;
-            window.openMediaPreview({
-                images: [{
-                    src: "assets/easter-eggs/nyapider.gif",
-                    alt: "Hajimi",
-                    caption: "別再按了><",
-                    downloadName: "下載下來做什麼？！？！.gif"
-                }
-                ]
-            });
-        } else {
-            resetTimer = setTimeout(() => { count = 0; }, RESET_DELAY);
-        }
-    });
-})();
-
 // Limo 設定圖 OF
 (function () {
     const link = document.getElementById("chara1FursonaOF");
@@ -507,72 +475,75 @@
 (function () {
     const songs = [
         // MSTP
-        { artist: "C418", title: "Alpha", url: "https://youtu.be/q6o7qpPHd7g", from: "Minecraft", source: "YouTube" },
-        { artist: "KIVΛ, Ice", title: "͟͝͞Ⅱ́̕", url: "https://youtu.be/H2k7TMT3ouA", from: "Cytus II", source: "YouTube" },
-        { artist: "KIVΛ", title: "The Whole Rest", url: "https://youtu.be/TWqMQeVqqnA", from: "Cytus II", source: "YouTube" },
-        { artist: "Theatrum Aeternum", title: "acta est fabula, plaudite", url: "https://youtu.be/TPTsSu77MKI", from: "vivid/stasis", source: "YouTube" },
-        { artist: "eicateve", title: "R.I.P.", url: "https://youtu.be/QJakdR6FWdg", source: "YouTube" },
-        { artist: "Poppin'Party", title: "Returns", url: "https://youtu.be/zWKV5yudE18", from: "BanG Dream!", source: "YouTube" },
-        { artist: "ryo (supercell)", title: "ODDS&ENDS", url: "https://youtu.be/HUzLUGKwQJc", source: "YouTube", unofficial: true },
-        { artist: "Ice", title: "L", url: "https://youtu.be/ZXy_1LA-RlA", source: "YouTube" },
-        { artist: "Ice", title: "L2 -Ascension- (Act 1)", url: "https://youtu.be/lAkUpYCUXCU", source: "YouTube", unofficial: true },
-        { artist: "Ice", title: "L2 -Ascension- (Act 2)", url: "https://youtu.be/8nF7MU5iGng", source: "YouTube", unofficial: true },
-        { artist: "Toby Fox", title: "MEGALOVANIA", url: "https://youtu.be/KK3KXAECte4", from: "UNDERTALE", source: "YouTube" },
-        { artist: "ああああ", title: "トゥルエンド", url: "https://youtu.be/hK7Inl9Rark", from: "でびるコネクショん", source: "YouTube" },
-        { artist: "Sta", title: "Incyde", url: "https://youtu.be/-1OiVXRGE-U", from: "Cytus II", source: "YouTube" },
-        { artist: "繊(Apo11o program vs. お月さま交響曲) ft.じまんぐ", title: "paradigm-paragramme-program", url: "https://youtu.be/8CNl8DZMAL0", from: "Cytus II", source: "YouTube" },
-        { artist: "Knighthood", title: "Super Universe (Knighthood Remix)", url: "https://youtu.be/v7rqB-o52tE", source: "YouTube", unofficial: true },
-        { artist: "technoplanet", title: "XYZ", url: "https://youtu.be/ZYXFo637GNc", from: "Cytus II", source: "YouTube" },
-        { artist: "Team Grimoire", title: "Grievous Lady", url: "https://youtu.be/QXeaLw2s-Wo", from: "Arcaea", source: "YouTube" },
-        { artist: "Team Grimoire", title: "Rrhar'il", url: "https://youtu.be/Hwny8gQRYZA", from: "Phigros", source: "YouTube" },
-        { artist: "Cheetah Mobile", title: "Neon", url: "https://youtu.be/pJIh0KPl98s", from: "Rolling Sky", source: "YouTube" },
-        { artist: "Dimrain47", title: "At the Speed of Light", url: "https://youtu.be/1Zrq8FiKS6A", source: "YouTube" },
-        { artist: "cYsmix", title: "Peer Gynt", url: "https://youtu.be/w4dLTLW6dJ0", source: "YouTube", unofficial: true },
-        { artist: "USAO & Camellia", title: "Möbius", url: "https://youtu.be/2fsZdfixj60", from: "WACCA", source: "YouTube" },
-        { artist: "Antistar feat. Ctymax", title: "Class Memories", url: "https://youtu.be/3Ka6yPBCs5A", source: "YouTube", unofficial: true },
-        { artist: "uma vs. モリモリあつし", title: "Re:End of a Dream", url: "https://youtu.be/ayg2A2JoRzg", source: "YouTube" },
+        { artist: "C418", title: "Alpha", url: "https://youtu.be/q6o7qpPHd7g", from: "Minecraft", platform: "YouTube" },
+        { artist: "KIVΛ, Ice", title: "͟͝͞Ⅱ́̕", url: "https://youtu.be/H2k7TMT3ouA", from: "Cytus II", platform: "YouTube" },
+        { artist: "KIVΛ", title: "The Whole Rest", url: "https://youtu.be/TWqMQeVqqnA", from: "Cytus II", platform: "YouTube" },
+        { artist: "log()", title: "SELF", url: "https://youtu.be/q7PXMBjTVLc", from: "vivid/stasis", platform: "YouTube" },
+        { artist: "Theatrum Aeternum", title: "acta est fabula, plaudite", url: "https://youtu.be/TPTsSu77MKI", from: "vivid/stasis", platform: "YouTube" },
+        { artist: "eicateve", title: "R.I.P.", url: "https://youtu.be/QJakdR6FWdg", platform: "YouTube" },
+        { artist: "Poppin'Party", title: "Returns", url: "https://youtu.be/zWKV5yudE18", from: "BanG Dream!", platform: "YouTube" },
+        { artist: "ryo (supercell)", title: "ODDS&ENDS", url: "https://youtu.be/HUzLUGKwQJc", platform: "YouTube", unofficial: true },
+        { artist: "Ice", title: "L", url: "https://youtu.be/ZXy_1LA-RlA", platform: "YouTube" },
+        { artist: "Ice", title: "L2 -Ascension- (Act 1)", url: "https://youtu.be/lAkUpYCUXCU", platform: "YouTube", unofficial: true },
+        { artist: "Ice", title: "L2 -Ascension- (Act 2)", url: "https://youtu.be/8nF7MU5iGng", platform: "YouTube", unofficial: true },
+        { artist: "Toby Fox", title: "MEGALOVANIA", url: "https://youtu.be/KK3KXAECte4", from: "UNDERTALE", platform: "YouTube" },
+        { artist: "ああああ", title: "トゥルエンド", url: "https://youtu.be/hK7Inl9Rark", from: "でびるコネクショん", platform: "YouTube" },
+        { artist: "Sta", title: "Incyde", url: "https://youtu.be/-1OiVXRGE-U", from: "Cytus II", platform: "YouTube" },
+        { artist: "繊(Apo11o program vs. お月さま交響曲) ft.じまんぐ", title: "paradigm-paragramme-program", url: "https://youtu.be/8CNl8DZMAL0", from: "Cytus II", platform: "YouTube" },
+        { artist: "Knighthood", title: "Super Universe (Knighthood Remix)", url: "https://youtu.be/v7rqB-o52tE", platform: "YouTube", unofficial: true },
+        { artist: "technoplanet", title: "XYZ", url: "https://youtu.be/ZYXFo637GNc", from: "Cytus II", platform: "YouTube" },
+        { artist: "Team Grimoire", title: "Grievous Lady", url: "https://youtu.be/QXeaLw2s-Wo", from: "Arcaea", platform: "YouTube" },
+        { artist: "Team Grimoire", title: "Rrhar'il", url: "https://youtu.be/Hwny8gQRYZA", from: "Phigros", platform: "YouTube" },
+        { artist: "Cheetah Mobile", title: "Neon", url: "https://youtu.be/pJIh0KPl98s", from: "Rolling Sky", platform: "YouTube" },
+        { artist: "Dimrain47", title: "At the Speed of Light", url: "https://youtu.be/1Zrq8FiKS6A", platform: "YouTube" },
+        { artist: "cYsmix", title: "Peer Gynt", url: "https://youtu.be/w4dLTLW6dJ0", platform: "YouTube", unofficial: true },
+        { artist: "USAO & Camellia", title: "Möbius", url: "https://youtu.be/2fsZdfixj60", from: "WACCA", platform: "YouTube" },
+        { artist: "Antistar feat. Ctymax", title: "Class Memories", url: "https://youtu.be/3Ka6yPBCs5A", platform: "YouTube", unofficial: true },
+        { artist: "uma vs. モリモリあつし", title: "Re:End of a Dream", url: "https://youtu.be/ayg2A2JoRzg", platform: "YouTube" },
 
         // 突然喜歡的歌
-        { artist: "A-One", title: "Idoratrize World", url: "https://youtu.be/n8vn1iFDhAs", source: "YouTube" },
-        { artist: "上海アリス幻樂団", title: "偶像に世界を委ねて　〜 Idoratrize World", url: "https://youtu.be/7DF5wIPlvq0", from: "東方鬼形獣 〜 Wily Beast and Weakest Creature.", source: "YouTube" },
-        { artist: "101-202-404", title: "小悪魔×3の大脫走！？", url: "https://youtu.be/HCgs32kX8eQ", from: "Cytus II", source: "YouTube", unofficial: true },
-        { artist: "黒皇帝", title: "Galaxy Collapse", url: "https://youtu.be/VJFNcHgQ4HM", source: "YouTube" },
-        { artist: "黒皇帝 vs MIssionary", title: "Deus Judicium", url: "https://youtu.be/CZJoFLSe9Ao", from: "Rotaeno", source: "YouTube" },
-        { artist: "NeLiME", title: "CODE NAME : ZERO", url: "https://youtu.be/26nQsUdhBNQ", from: "Cytus", source: "YouTube" },
-        { artist: "log()", title: "SELF", url: "https://youtu.be/q7PXMBjTVLc", from: "vivid/stasis", source: "YouTube" },
-        { artist: "Ayatsugu_Revolved", title: "100sec Cat Dreams", url: "https://youtu.be/zBlmtNKgrk0", from: "Cytus II", source: "YouTube", unofficial: true },
-        { artist: "It's MyGO!!!!!", title: "詩超絆", url: "https://youtu.be/wJ-OebTVyvk", from: "BanG Dream!", source: "YouTube" },
-        { artist: "Poppin'Party", title: "Dreamers Go!", url: "https://youtu.be/VigNV3bsE_k", from: "BanG Dream!", source: "YouTube" },
-        { artist: "ああああ", title: "でびるコネクショん", url: "https://youtu.be/aQx9OjvQZEo", from: "でびるコネクショん", source: "YouTube" },
-        { artist: "KIVΛ", title: "Used to be", url: "https://youtu.be/hGaJNvkRfo0", from: "Cytus II", source: "YouTube" },
-        { artist: "MELOIMAGE", title: "Imprint", url: "https://youtu.be/mTcFEVeVoDs", from: "Cytus II", source: "YouTube", unofficial: true },
-        { artist: "Apo11o program", title: "Re:The END -再-", url: "https://youtu.be/gnt9Bnei2is", from: "Cytus II", source: "YouTube" },
-        { artist: 'NOMA w/ Apo11o"ALGIEBA"program', title: "LAST Re;SØRT", url: "https://youtu.be/2a0wyR-Hu1Y", from: "RAVON", source: "YouTube" },
-        { artist: "Tobu", title: "Higher", url: "https://youtu.be/blA7epJJaR4", source: "YouTube" },
-        { artist: "ユリイ・カノン", title: "スーサイドパレヱド", url: "https://youtu.be/7awIdGqyr40", source: "YouTube" },
-        { artist: "Roselia", title: "Neo-Aspect", url: "https://youtu.be/03iVXFZ8jrs", from: "BanG Dream!", source: "YouTube" },
-        { artist: "RAISE A SUILEN", title: "DEAD HEAT BEAT", url: "https://youtu.be/2gJfjLGCf9U", from: "BanG Dream!", source: "YouTube" },
-        { artist: "DECO*27", title: "ヴァンパイア", url: "https://youtu.be/e1xCOsgWG0M", source: "YouTube" },
-        { artist: "Tsukasa", title: "Stardust Sphere", url: "https://youtu.be/f9XYU172ImI", from: "Cytus", source: "YouTube" },
-        { artist: "Ice", title: "iL", url: "https://youtu.be/ilLGb4b7Twc", from: "Cytus II", source: "YouTube" },
-        { artist: "Y&Co.", title: "Daisuke", url: "https://www.nicovideo.jp/watch/sm13256898", source: "ニコニコ" },
-        { artist: "xi", title: "Xaleid◆scopiX", url: "https://youtu.be/-PTe8zkYt9A", from: "maimai でらっくす", source: "YouTube" },
-        { artist: "Shu feat. 天羽しろっぷ", title: "殿ッ！？ご乱心！？", url: "https://youtu.be/U2i_IuAB6wo", from: "maimai でらっくす", source: "YouTube" },
-        { artist: "ああああ", title: "優しさに触れて", url: "https://youtu.be/f8qaWMjyVWU", from: "でびるコネクショん", source: "YouTube" },
-        { artist: "Quree", title: "HTTPS", url: "https://youtu.be/dQZ14TWuhi0", source: "YouTube" },
-        { artist: "Kry.exe vs. Ganymede", title: "First Breath", url: "https://youtu.be/fJu8paff0Xw", from: "vivid/stasis", source: "YouTube" },
-        { artist: "ああああ", title: "そうして明日も続いていく", url: "https://youtu.be/Go1R4PHAnec", from: "でびるコネクショん", source: "YouTube" },
-        { artist: "Consider", title: "夏目", url: "https://www.bilibili.com/video/BV1FCVAzZEcE/", from: "この雪が解けるまで", source: "Bilibili", unofficial: true },
-        { artist: 'Apo11o"EQUATOR"program vs.Nightster', title: "ΛVeS", url: "https://youtu.be/C_AXYPvm5V0", from: "DEEMO II", source: "YouTube" },
-        { artist: "ああああ", title: "ここが居場所", url: "https://youtu.be/yP29FeiZlpk", from: "でびるコネクショん", source: "YouTube" },
-        { artist: "BTB", title: "Weißer Flügel", url: "https://www.nicovideo.jp/watch/sm35039151", source: "ニコニコ" },
-        { artist: "大国奏音", title: "封焔の135秒", url: "https://youtu.be/gSaVkMJpdcQ", from: "maimai でらっくす", source: "YouTube" },
-        { artist: "Kry.exe-天利", title: "Sigma (Haocore Mix) ~ Regrets of the Yellow Tulip ~", url: "https://youtu.be/8iwB9-9kKRQ", from: "Phigros", source: "YouTube", unofficial: true },
-        { artist: "siromaru + cranky", title: "conflict", url: "https://youtu.be/e8HRTlkGD4Q", source: "YouTube" },
-        { artist: "HOYO-MiX", title: "Hope Is the Thing With Feathers", url: "https://youtu.be/f_E7TX8DSmM", from: "崩壊：スターレイル", source: "YouTube" },
-        { artist: "花譜", title: "邂逅", url: "https://youtu.be/Q5DlTbOeNdQ", source: "YouTube" }
+        { artist: "A-One", title: "Idoratrize World", url: "https://youtu.be/n8vn1iFDhAs", platform: "YouTube" },
+        { artist: "上海アリス幻樂団", title: "偶像に世界を委ねて　〜 Idoratrize World", url: "https://youtu.be/7DF5wIPlvq0", from: "東方鬼形獣 〜 Wily Beast and Weakest Creature.", platform: "YouTube" },
+        { artist: "101-202-404", title: "小悪魔×3の大脫走！？", url: "https://youtu.be/HCgs32kX8eQ", from: "Cytus II", platform: "YouTube", unofficial: true },
+        { artist: "黒皇帝", title: "Galaxy Collapse", url: "https://youtu.be/VJFNcHgQ4HM", platform: "YouTube" },
+        { artist: "黒皇帝 vs MIssionary", title: "Deus Judicium", url: "https://youtu.be/CZJoFLSe9Ao", from: "Rotaeno", platform: "YouTube" },
+        { artist: "NeLiME", title: "CODE NAME : ZERO", url: "https://youtu.be/26nQsUdhBNQ", from: "Cytus", platform: "YouTube" },
+        { artist: "Ayatsugu_Revolved", title: "100sec Cat Dreams", url: "https://youtu.be/zBlmtNKgrk0", from: "Cytus II", platform: "YouTube", unofficial: true },
+        { artist: "It's MyGO!!!!!", title: "詩超絆", url: "https://youtu.be/wJ-OebTVyvk", from: "BanG Dream!", platform: "YouTube" },
+        { artist: "Poppin'Party", title: "Dreamers Go!", url: "https://youtu.be/VigNV3bsE_k", from: "BanG Dream!", platform: "YouTube" },
+        { artist: "ああああ", title: "でびるコネクショん", url: "https://youtu.be/aQx9OjvQZEo", from: "でびるコネクショん", platform: "YouTube" },
+        { artist: "KIVΛ", title: "Used to be", url: "https://youtu.be/hGaJNvkRfo0", from: "Cytus II", platform: "YouTube" },
+        { artist: "MELOIMAGE", title: "Imprint", url: "https://youtu.be/mTcFEVeVoDs", from: "Cytus II", platform: "YouTube", unofficial: true },
+        { artist: "Apo11o program", title: "Re:The END -再-", url: "https://youtu.be/gnt9Bnei2is", from: "Cytus II", platform: "YouTube" },
+        { artist: 'NOMA w/ Apo11o"ALGIEBA"program', title: "LAST Re;SØRT", url: "https://youtu.be/2a0wyR-Hu1Y", from: "RAVON", platform: "YouTube" },
+        { artist: "Tobu", title: "Higher", url: "https://youtu.be/blA7epJJaR4", platform: "YouTube" },
+        { artist: "ユリイ・カノン", title: "スーサイドパレヱド", url: "https://youtu.be/7awIdGqyr40", platform: "YouTube" },
+        { artist: "Roselia", title: "Neo-Aspect", url: "https://youtu.be/03iVXFZ8jrs", from: "BanG Dream!", platform: "YouTube" },
+        { artist: "RAISE A SUILEN", title: "DEAD HEAT BEAT", url: "https://youtu.be/2gJfjLGCf9U", from: "BanG Dream!", platform: "YouTube" },
+        { artist: "DECO*27", title: "ヴァンパイア", url: "https://youtu.be/e1xCOsgWG0M", platform: "YouTube" },
+        { artist: "Tsukasa", title: "Stardust Sphere", url: "https://youtu.be/f9XYU172ImI", from: "Cytus", platform: "YouTube" },
+        { artist: "Ice", title: "iL", url: "https://youtu.be/ilLGb4b7Twc", from: "Cytus II", platform: "YouTube" },
+        { artist: "Y&Co.", title: "Daisuke", url: "https://www.nicovideo.jp/watch/sm13256898", platform: "ニコニコ" },
+        { artist: "xi", title: "Xaleid◆scopiX", url: "https://youtu.be/-PTe8zkYt9A", from: "maimai でらっくす", platform: "YouTube" },
+        { artist: "Shu feat. 天羽しろっぷ", title: "殿ッ！？ご乱心！？", url: "https://youtu.be/U2i_IuAB6wo", from: "maimai でらっくす", platform: "YouTube" },
+        { artist: "ああああ", title: "優しさに触れて", url: "https://youtu.be/f8qaWMjyVWU", from: "でびるコネクショん", platform: "YouTube" },
+        { artist: "Quree", title: "HTTPS", url: "https://youtu.be/dQZ14TWuhi0", platform: "YouTube" },
+        { artist: "Kry.exe vs. Ganymede", title: "First Breath", url: "https://youtu.be/fJu8paff0Xw", from: "vivid/stasis", platform: "YouTube" },
+        { artist: "ああああ", title: "そうして明日も続いていく", url: "https://youtu.be/Go1R4PHAnec", from: "でびるコネクショん", platform: "YouTube" },
+        { artist: "Consider", title: "夏目", url: "https://www.bilibili.com/video/BV1FCVAzZEcE/", from: "この雪が解けるまで", platform: "Bilibili", unofficial: true },
+        { artist: 'Apo11o"EQUATOR"program vs.Nightster', title: "ΛVeS", url: "https://youtu.be/C_AXYPvm5V0", from: "DEEMO II", platform: "YouTube" },
+        { artist: "ああああ", title: "ここが居場所", url: "https://youtu.be/yP29FeiZlpk", from: "でびるコネクショん", platform: "YouTube" },
+        { artist: "BTB", title: "Weißer Flügel", url: "https://www.nicovideo.jp/watch/sm35039151", platform: "ニコニコ" },
+        { artist: "大国奏音", title: "封焔の135秒", url: "https://youtu.be/gSaVkMJpdcQ", from: "maimai でらっくす", platform: "YouTube" },
+        { artist: "Kry.exe-天利", title: "Sigma (Haocore Mix) ~ Regrets of the Yellow Tulip ~", url: "https://youtu.be/8iwB9-9kKRQ", from: "Phigros", platform: "YouTube", unofficial: true },
+        { artist: "siromaru + cranky", title: "conflict", url: "https://youtu.be/e8HRTlkGD4Q", platform: "YouTube" },
+        { artist: "HOYO-MiX", title: "Hope Is the Thing With Feathers", url: "https://youtu.be/f_E7TX8DSmM", from: "崩壊：スターレイル", platform: "YouTube" },
+        { artist: "花譜", title: "邂逅", url: "https://youtu.be/Q5DlTbOeNdQ", platform: "YouTube" },
+        { artist: "wowaka", title: "ローリンガール", url: "https://youtu.be/vnw8zURAxkU", platform: "YouTube" },
+        { artist: "Bon Jovi", title: "It's My Life", url: "https://youtu.be/vx2u5uUu3DE", platform: "YouTube" },
+        { artist: "MyReminiscence", title: "Memory", url: "https://youtu.be/alfnHgCTWr0", from: "Ib", platform: "YouTube" }
 
-        // { artist: "", title: "", url: "", from: "", source: "" }
+        // { artist: "", title: "", url: "", from: "", platform: "" }
     ];
 
     const getDailySeed = () => {
@@ -637,10 +608,10 @@
         }
 
         // 來源平台
-        const sourceDiv = document.createElement("p");
-        sourceDiv.style.margin = "0.3em 0";
-        sourceDiv.textContent = `平台：${dailySong.source}`;
-        container.appendChild(sourceDiv);
+        const platformDiv = document.createElement("p");
+        platformDiv.style.margin = "0.3em 0";
+        platformDiv.textContent = `平台：${dailySong.platform}`;
+        container.appendChild(platformDiv);
     };
 
     const dailySong = selectDailySong();
